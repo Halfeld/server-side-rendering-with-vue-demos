c991faede922304345a903d516e6572afba0bbbf
Igor Luiz <igor@devstacks.com.br>
2018-04-26 13:56:16 -0300

    Add not ssr example

---
+++ app-without-ssr/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div v-html="blog"></div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import MarkdownIt from 'markdown-it'
+
+export default {
+  name: 'app',
+  data: () => ({
+    blog: ''
+  }),
+  async mounted () {
+    const { data: response } = await axios.get('static/my-post.md')
+    const md = new MarkdownIt()
+    this.blog = md.render(response)
+  }
+}
+</script>
