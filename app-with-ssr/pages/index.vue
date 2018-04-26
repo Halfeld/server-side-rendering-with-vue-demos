951f19c6b9cfecf7988eb3caed62228b5443f09e
Igor Luiz <igor@devstacks.com.br>
2018-04-26 14:11:04 -0300

    Add ssr example

---
+++ app-with-ssr/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-html="blog">
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import MarkdownIt from 'markdown-it'
+
+export default {
+  name: 'App',
+  data: () => ({
+    blog: ''
+  }),
+  async asyncData () {
+    const { data: response } = await axios.get('http://localhost:3000/my-post.md')
+    const md = new MarkdownIt()
+    const blog = md.render(response)
+
+    return {
+      blog
+    }
+  }
+}
+</script>
---
+++ app-with-ssr/static/my-post.md
@@ -0,0 +1,4 @@
+
+# This is a Header
+
+This is a paragrapher with **bold** and _italic_ text.
